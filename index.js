f684d56c2ed63566b340bb60d162f9d3f1373291
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-04-22 10:50:49 +0200

    list add + file upload

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+document.getElementById('yamlFile').addEventListener('change', function () {
+  const fileInput = this;
+  const file = fileInput.files[0];
+  const fileList = document.getElementById('fileList');
+
+  fileList.innerHTML = '';
+
+  if (file) {   
+    const listItem = document.createElement('li');
+    listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+    listItem.textContent = file.name;
+
+    const fileSize = document.createElement('span');
+    fileSize.className = "badge bg-primary rounded-pill";
+    fileSize.textContent = `${(file.size / 1024).toFixed(2)} KB`;
+    
+    listItem.appendChild(fileSize);
+    fileList.appendChild(listItem);
+    fileList.classList.remove('d-none');
+  } else {
+    fileList.classList.add('d-none');
+  }
+});
+
 document.getElementById('validateBtn').addEventListener('click', function () {
   const fileInput = document.getElementById('yamlFile');
   const file = fileInput.files[0];
